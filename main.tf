479e88d54c0c8b9338bd8acb7cefed36cc6db27a
Lucas <lucas_fegue@hotmail.com>
2022-10-20 11:59:05 -0300

    Update terraform

--- main.tf
+++ main.tf
@@ -1,44 +1,57 @@
 terraform {
-    required_providers {
-      digitalocean = {
-          source = "digitalocean/digitalocean"
-          version = "~> 2.0"
-      }
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
     }
+  }
 }
 
 provider "digitalocean" {
-    token = var.do_token
+  token = var.do_token
 }
 
-resource "digitalocean_kubernetes_cluster" "k8s_iniciativa" {
-    name = var.k8s_name
-    region = var.region 
-    version = "1.22.8-do.1"
-
-    node_pool {
-        name = "default"
-        size = "s-2vcpu-4gb"
-        node_count = 2
-    }    
+resource "digitalocean_droplet" "jenkins" {
+  image    = "ubuntu-22-04-x64"
+  name     = "jenkins"
+  region   = var.region
+  size     = "s-2vcpu-2gb"
+  ssh_keys = [data.digitalocean_ssh_key.ssh_key.id]
 }
 
-resource "digitalocean_kubernetes_node_pool" "node_premium" {
-    cluster_id = digitalocean_kubernetes_cluster.k8s_iniciativa.id
-     name = "premium"
-     size = "s-4vcpu-8gb"
+data "digitalocean_ssh_key" "ssh_key" {
+  name = var.ssh_key_name
+}
+
+resource "digitalocean_kubernetes_cluster" "k8s" {
+  name    = "k8s"
+  region  = var.region
+  version = "1.24.4-do.0"
+
+  node_pool {
+    name       = "default"
+    size       = "s-2vcpu-2gb"
     node_count = 2
+  }
 }
 
-variable "do_token" {}
-variable "k8s_name" {}
-variable "region" {}
-
-output "kube_endpoint" {
-    value = digitalocean_kubernetes_cluster.k8s_iniciativa.endpoint
+variable "region" {
+  default = ""
 }
 
-resource "local_file" "kube_config" {
-    content = digitalocean_kubernetes_cluster.k8s_iniciativa.kube_config.0.raw_config
-    filename = "kube_config.yaml"
+variable "do_token" {
+  default = ""
+}
+
+variable "ssh_key_name" {
+  default = ""
+}
+
+output "jenkins_ip" {
+  value = digitalocean_droplet.jenkins.ipv4_address
+}
+
+resource "local_file" "foo" {
+  content  = digitalocean_kubernetes_cluster.k8s.kube_config.0.raw_config
+  filename = "kube_config.yaml"
 }
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,3 +1,3 @@
-do_token = "dop_v1_DfCnYD5WCZbVghUFoSKa91ifiB4U5D3F4EdSTasYwPl6EaL0Mi"
-k8s_name = "k8s_iniciativa-devops"
-region = "nyc1"
+do_token     = "dop_v1_DfCnYD5WCZbVghUFoSKa91ifiB4U5D3F4EdSTasYwPl6EaL0Mi"
+ssh_key_name = "Jornada"
+region       = "nyc1"
